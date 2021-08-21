b0b329c408b53dbf61aec0bec37c7a8df9260645
yoshiyasugimoto <gabesaiti@gmail.com>
2021-08-21 23:42:36 +0900

    access URL

--- auto_browser.py
+++ auto_browser.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 import os 
-
+import time
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ['enable-automation'])
 CHROME_PROFILE = os.environ.get("chrome_profile")
@@ -12,3 +12,8 @@ driver = webdriver.Chrome(executable_path=os.environ.get("chrome_driver_path"),o
 
 URL = os.getenv("access", "https://github.com")
 driver.get(URL)
+time.sleep(5)
+messageBody1 = driver.find_element_by_id("messageBody")
+messageBody2 = driver.find_element_by_id("messageBody")
+messageBody3 = driver.find_element_by_id("messageBody")
+
