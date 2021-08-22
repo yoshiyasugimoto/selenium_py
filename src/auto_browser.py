1f7f3f3ec893c6d972c4f6fb5b749e8ecdeddd85
yoshiyasugimoto <gabesaiti@gmail.com>
2021-08-22 16:14:05 +0900

    Fix

--- src/auto_browser.py
+++ src/auto_browser.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 
+import datetime
 import os 
-import time
+
 options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
+# options.add_argument('window-size=1366x768')
 options.add_experimental_option("excludeSwitches", ['enable-automation'])
+
 CHROME_PROFILE = os.environ.get("chrome_profile")
 if CHROME_PROFILE:
     options.add_argument(f'--user-data-dir={CHROME_PROFILE}')
@@ -12,13 +16,27 @@ driver = webdriver.Chrome(executable_path=os.environ.get("chrome_driver_path"),o
 
 URL = os.getenv("access", "https://github.com")
 driver.get(URL)
-time.sleep(5)
+driver.maximize_window()
+driver.implicitly_wait(10)
 
 if "teams" not in os.getenv("access"):
+    print("Access to other than teams")
     driver.close()
+
+
+if "https://login.live.com/" in driver.current_url:
+    print("Login in")
+    breakpoint()
+    
 
 message_body = driver.find_elements_by_class_name('message-body')
 if message_body:
+    today = datetime.date.today()
+    today_str = today.strftime("%m/%d") if today.strftime("%m/%d")[0] != "0" else today.strftime("%m/%d")[1:]
+    print(today_str)
     for i in message_body:
         print(i.text)
     driver.close()
+
+else:
+    print("Cannot find message_body")
